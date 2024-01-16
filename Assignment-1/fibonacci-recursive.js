790b3dd8523eab0290b3ddf231a198aca975bb91
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2024-01-16 00:22:46 -0800

    Clean code

--- Assignment-1/fibonacci-recursive.js
+++ Assignment-1/fibonacci-recursive.js
@@ -9,7 +9,7 @@ const fibsRec = (n) => {
     return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
 };
 
-console.log(fibsRec(1)); // Expected Result: [0]
-console.log(fibsRec(2)); // Expected Result: [0, 1]
-console.log(fibsRec(3)); // Expected Result: [0, 1, 1]
-console.log(fibsRec(8)); // Expected Result: [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsRec(1)); // [0]
+console.log(fibsRec(2)); // [0, 1]
+console.log(fibsRec(3)); // [0, 1, 1]
+console.log(fibsRec(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
